a73fcd160f600e648e5384fd4f209db6b7393798
gabriel <gabrielfurtunatofranca@gmail.com>
2022-01-18 20:04:38 -0300

    primeiro commit

---
+++ main.js
@@ -0,0 +1,43 @@
+'use strict';
+
+const clearForm = (endereco) =>{
+    document.getElementById('endereco').value = '';
+    document.getElementById('bairro').value = '';
+    document.getElementById('cidade').value = '';
+    document.getElementById('estado').value = '';
+}
+
+
+const fillForm = (endereco) =>{
+    document.getElementById('endereco').value = endereco.logradouro;
+    document.getElementById('bairro').value = endereco.bairro;
+    document.getElementById('cidade').value = endereco.localidade;
+    document.getElementById('estado').value = endereco.uf;
+}
+
+
+const isNumber = (numero) => /^[0-9]+$/.test(numero);
+
+const cepValido = (cep) => cep.length == 8 && isNumber(cep); 
+
+const searchcep = async() => {
+    clearForm();
+    
+    const cep = document.getElementById('cep').value.replace("-","");
+    const url = `https://viacep.com.br/ws/${cep}/json/`;
+    if (cepValido(cep)){
+        const dados = await fetch(url);
+        const endereco = await dados.json();
+        if (endereco.hasOwnProperty('erro')){
+            document.getElementById('endereco').value = 'CEP não encontrado!';
+        }else {
+            fillForm(endereco);
+        }
+    }else{
+        document.getElementById('endereco').value = 'CEP incorreto!';
+    }
+     
+}
+
+document.getElementById('cep')
+        .addEventListener('focusout',searchcep);
